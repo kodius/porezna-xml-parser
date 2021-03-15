af406ff2a63b4683b97a9e03849f4278d2829676
mateo9 <mvukusic199@gmail.com>
2021-03-15 10:13:37 +0100

    Parse xml

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,26 @@
 module ApplicationHelper
+  class << self
+    def reduce_same_keys(arr)
+      remove_id_from_xml(arr)
+      grouping_data_from_file(arr).each_with_index.map do |a, index|
+        a.flatten.reduce do |acc,h|
+          acc.merge(h) do |key, v1, v2|
+            keys_to_reduce.include?(key) ? v1.to_f + v2.to_f : v2
+          end
+        end
+      end
+    end
+
+    def keys_to_reduce
+      %w(R8 R9 R10 R11 R12 R13 R14 R15 R16 R17 R18)
+    end
+
+    def remove_id_from_xml(arr)
+      arr.each { |h| h.delete('R1') }
+    end
+
+    def grouping_data_from_file(arr)
+      arr.group_by {|g| [g["R2"], g["R3"], g["R7"]] }.values
+    end
+  end
 end
---
+++ app/services/nokogiri_services.rb
@@ -0,0 +1,30 @@
+class NokogiriServices
+   def self.call(xml)
+      @doc = Nokogiri::XML(xml, &:noblanks)
+      @r_elements = @doc.search("R")
+      arr = detach_racun_data(@r_elements)
+      finish_xml_parsing(arr)
+   end
+
+   def self.detach_racun_data(data)
+     arr = []
+     data.map do |k|
+       h = {}
+       k.children.map do |v|
+         h.merge!(v.name => v.children.to_s) if v.class.name == 'Nokogiri::XML::Element'
+       end
+       arr << h
+     end
+     ApplicationHelper.reduce_same_keys(arr)
+   end
+
+   def self.finish_xml_parsing(reduced_array)
+     @doc.search('Racuni').remove
+     @doc.at('Tijelo').prepend_child "<Racuni></Racuni>"
+     reduced_array.each_with_index do |h, index|
+       string = "<R1>#{index + 1}</R1>" + h.map {|key,value| "<#{key}>#{value}</#{key}>" }.join('')
+       @doc.at('Racuni').add_child "<R>#{string}</R>"
+     end
+     @doc
+   end
+end
