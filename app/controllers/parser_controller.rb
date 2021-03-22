8eb55af87ae4184b561633bacf65dc97b83d3f36
mateo9 <mvukusic199@gmail.com>
2021-03-22 12:22:46 +0100

    WIP

--- app/controllers/parser_controller.rb
+++ app/controllers/parser_controller.rb
@@ -1,9 +1,13 @@
 class ParserController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index; end
 
   def upload
+    sleep 5
     read_xml = params[:fileUpload].read
     doc = NokogiriServices.call(read_xml)
+    headers['Content-Type'] = "application/octet-stream"
     send_data(
       doc.to_xml,
       filename: filename,
