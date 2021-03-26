e680b66b1c32aa2f58716b2c6d36924d44e83261
mateo9 <mvukusic199@gmail.com>
2021-03-26 09:20:29 +0100

    WIP

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'parser#index'
 
   post '/upload-xml' => 'parser#upload'
+  get '/download-xml' => 'download#upload'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
