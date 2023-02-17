4026973a1efc31bcd2dbf43dedd50489695dd91e
mateo@kodius.com <mateo@kodius.com>
2023-02-17 15:15:33 +0100

    .

--- app/services/nokogiri_services.rb
+++ app/services/nokogiri_services.rb
@@ -4,6 +4,15 @@ class NokogiriServices
   def self.call(xml)
     @doc = Nokogiri::XML(xml, &:noblanks)
     @r_elements = @doc.search('R')
+    @obrazac = @doc.search('ObrazacURA')
+    root = @doc.root
+    @doc.remove_namespaces!
+    root['xmlns'] = 'http://e-porezna.porezna-uprava.hr/sheme/zahtjevi/ObrazacURA/v2-0'
+    root['xmlns:meta'] = 'http://e-porezna.porezna-uprava.hr/sheme/Metapodaci/v2-0'
+    root['xmlns:xsi'] = 'http://www.w3.org/2001/XMLSchema-instance'
+    @obrazac[0].attributes['verzijaSheme'].value = '2.0'
+    root['xsi:schemaLocation'] = 'http://e-porezna.porezna-uprava.hr/sheme/zahtjevi/ObrazacURA/v2-0 ObrazacURA-v2-0.xsd'
+    @doc.root.remove_attribute('schemaLocation')
     arr = detach_racun_data(@r_elements)
     finish_xml_parsing(arr)
   end
@@ -26,9 +35,9 @@ class NokogiriServices
     @doc.search('Racuni').remove
     @doc.at('Tijelo').prepend_child '<Racuni></Racuni>'
     reduced_array.sort_by { |hsh| hsh['R3'] }.each_with_index do |h, index|
-			mapa = h.to_a 
-			mapa.insert(6, ["R19", "0.00"])
-			sortirano = mapa.to_h
+      mapa = h.to_a
+      mapa.insert(6, ['R19', '0.00'])
+      sortirano = mapa.to_h
       string = "<R1>#{index + 1}</R1>" + sortirano.map { |key, value| "<#{key}>#{value}</#{key}>" }.join('')
       @doc.at('Racuni').add_child "<R>#{string}</R>"
     end
